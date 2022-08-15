c731392b9692c43c5e8c7019b13f391a0902840d
Charity <umercy001@gmail.com>
2022-08-15 23:06:58 +0100

    practical nft collection

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x894295bA8dE3C7534aB461C05b6522A81C90bc72";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
